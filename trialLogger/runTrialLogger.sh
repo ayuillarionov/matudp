69d772e818ef22aa4e8309cce1c3edca36203d38
Alexey Yu. Illarionov <ayuillarionov@ini.uzh.ch>
2020-03-15 11:59:22 +0100

    Add OSX specific binaries

--- trialLogger/runTrialLogger.sh
+++ trialLogger/runTrialLogger.sh
@@ -5,7 +5,7 @@
 #             <ayuillarionov@ini.uzh.ch>
 #
 # Created   : Tue 21 Mar 2017 06:13:47 PM CET
-# Modified  : Fri 19 Jul 2019 10:48:43 AM CEST
+# Modified  : Sun Mar 15 11:58:19 2020
 # Computer  : ZVPIXX
 # System    : Linux 4.4.0-67-lowlatency x86_64 x86_64
 #
@@ -15,6 +15,16 @@
 #
 # NOTE      : Check if firewall does not blocking the port: sudo ufw status
 # ---------------------------------------------------------
+OS=$(uname -s)
+case $OS in
+	'Linux')
+		OS=lin
+	;;
+	'Darwin')
+		OS=mac
+	;;
+	*) ;;
+esac
 
-bin/trialLogger -r 100.1.1.2:29001 -d /udpTrialLogger
+bin/trialLogger-$OS -r 100.1.1.2:29001 -d /udpTrialLogger
 
